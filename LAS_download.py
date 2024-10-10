c90da717fdbf42257975892a704970852b8a111b
Frerijs <edijs.freijs@gmail.com>
2024-10-10 10:20:19 +0300

    Update LAS_download.py

--- LAS_download.py
+++ LAS_download.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import geopandas as gpd
 import requests
+import zipfile
+import os
+
+# Funkcija ZIP faila izsaiņošanai
+def unzip_file(zip_file_path, extract_to):
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
 
 # Funkcija, lai lejupielādētu failu no saites
 def download_data(url, output_filename):
@@ -12,15 +19,21 @@ def download_data(url, output_filename):
     else:
         return "Lejuplāde neizdevās."
 
-# Ielādēt SHP failu bez ģeometrijas
+# Ceļš uz ZIP failu
+zip_file_path = 'LASMAP.zip'
+extract_to = 'extracted_shp'
+
+# Izsaiņot ZIP failu
+unzip_file(zip_file_path, extract_to)
+
+# Ceļš uz SHP failu pēc izsaiņošanas
+shp_file_path = os.path.join(extract_to, 'LASMAP.shp')
+
+# Ielādēt SHP failu
 @st.cache_data
 def load_shp_file(shp_file_path):
     return gpd.read_file(shp_file_path, ignore_geometry=True)
 
-# Ceļš uz SHP failu
-shp_file_path = 'path_to_your_shp/LASMAP.shp'
-
-# Ielādēt SHP failu
 gdf = load_shp_file(shp_file_path)
 
 # Izvēlēties poligonu, pēc kura lejuplādēt datus
