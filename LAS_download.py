378987ad7014c9fd3aa719e6c6c9ee3d15fed96c
Frerijs <edijs.freijs@gmail.com>
2024-10-10 11:06:04 +0300

    Update LAS_download.py

--- LAS_download.py
+++ LAS_download.py
@@ -1,33 +1,21 @@
-import streamlit as st
-import requests
+import gdown
 import zipfile
 import os
 
-def download_zip_from_github(url, output_filename):
-    headers = {
-        'User-Agent': 'Mozilla/5.0',
-        'Accept': 'application/vnd.github.v3.raw'
-    }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        with open(output_filename, 'wb') as f:
-            f.write(response.content)
-        return True
-    else:
-        st.error(f"Neizdevās lejupielādēt ZIP failu no GitHub. Statusa kods: {response.status_code}")
-        return False
+# Google Drive faila ID
+file_id = "FAILE_ID_NO_GOOGLE_DRIVE"
 
-# URL uz GitHub failu
-github_url = "https://github.com/Frerjs/LAS_download/raw/main/LASMAP.zip"
-output_zip_path = "LASMAP.zip"
+# Izveido tiešsaistes lejupielādes URL
+download_url = f"https://drive.google.com/uc?id={file_id}"
 
-# Lejupielādēt ZIP failu
-if download_zip_from_github(github_url, output_zip_path):
-    st.write(f"Fails veiksmīgi lejupielādēts: {output_zip_path}")
-    
-    # Izsaiņot ZIP failu
-    with zipfile.ZipFile(output_zip_path, 'r') as zip_ref:
-        zip_ref.extractall("extracted_files")
-        st.write("ZIP fails veiksmīgi izsaiņots!")
-else:
-    st.write("ZIP faila lejupielāde neizdevās.")
+# Norādi, kur saglabāt lejupielādēto failu
+output_path = "LASMAP.zip"
+
+# Lejupielādē ZIP failu no Google Drive
+gdown.download(download_url, output_path, quiet=False)
+
+# Izsaiņot ZIP failu
+with zipfile.ZipFile(output_path, 'r') as zip_ref:
+    zip_ref.extractall("extracted_files")
+
+print("ZIP fails veiksmīgi izsaiņots!")
