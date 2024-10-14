884df8b39051d6a5a3caa7f40c50fe726ee239bd
Frerijs <edijs.freijs@gmail.com>
2024-10-14 15:53:59 +0300

    Update LAS_download.py

--- LAS_download.py
+++ LAS_download.py
@@ -10,6 +10,7 @@ from tempfile import TemporaryDirectory
 google_sheet_csv_url = "https://docs.google.com/spreadsheets/d/1u-myVB6WYK0Zp18g7YDZt59AdrmHB0nA4rvQehYbcjg/export?format=csv"
 
 # Funkcija, lai iegūtu lietotāja datus no Google Sheets
+@st.cache_data
 def get_user_data():
     df = pd.read_csv(google_sheet_csv_url)
     df.columns = df.columns.str.strip()  # Noņem atstarpes kolonnu nosaukumiem
@@ -42,7 +43,7 @@ def create_open_all_links_button(links):
     <style>
     .button {
         display: inline-block;
-        background-color: #f44336;
+        background-color: #4CAF50;
         color: white;
         padding: 10px 20px;
         text-align: center;
@@ -71,7 +72,7 @@ def create_open_all_links_button(links):
         }
     }
     </script>
-    <button class="button" onclick="openAllLinks()">Lejuplādēt visus datus</button>
+    <button class="button" onclick="openAllLinks()">Lejupielādēt visus datus</button>
     </body></html>
     """
     return html_content
@@ -80,70 +81,95 @@ def create_open_all_links_button(links):
 file_id = "1Xo7gVZ2WOm6yWv6o0-jCs_OsVQZQdffQ"
 output_zip_path = "LASMAP.zip"
 
-# Pieteikšanās interfeiss
-st.title("Pieteikšanās")
+# Inicializē sesijas stāvokli, ja nav jau inicializēts
+if 'authenticated' not in st.session_state:
+    st.session_state.authenticated = False
+    st.session_state.username = ''
 
-username = st.text_input("Lietotājvārds")
-password = st.text_input("Parole", type="password")
+def login():
+    st.title("Pieteikšanās")
 
-if st.button("Pieslēgties"):
-    users = get_user_data()
-    if authenticate(username, password, users):
-        st.success("Veiksmīgi pieteicies!")
-        
-        # Progresjosla un lejupielādes process
-        progress_bar = st.progress(0)
-        progress_percentage = 0
+    username = st.text_input("Lietotājvārds")
+    password = st.text_input("Parole", type="password")
 
-        # Jaunais virsraksts
-        st.write("Lejupielādē LAS datu karšu lapas...")
+    if st.button("Pieslēgties"):
+        users = get_user_data()
+        if authenticate(username, password, users):
+            st.session_state.authenticated = True
+            st.session_state.username = username
+            st.success("Veiksmīgi pieteicies!")
+        else:
+            st.error("Nepareizs lietotājvārds vai parole.")
 
-        try:
-            # Lejupielādē ZIP failu no Google Drive
-            if download_from_google_drive(file_id, output_zip_path):
+def logout():
+    st.session_state.authenticated = False
+    st.session_state.username = ''
+    st.success("Jūs esat izlogojies.")
+
+def main_app():
+    st.title("LAS Datu Lejupielāde")
+
+    st.write(f"Veiksmīgi pieteicies, **{st.session_state.username}**!")
+
+    # Log-off poga
+    if st.button("Iziet"):
+        logout()
+        st.experimental_rerun()
+
+    # Progresjosla un lejupielādes process
+    progress_bar = st.progress(0)
+    progress_percentage = 0
+
+    # Jaunais virsraksts
+    st.write("Lejupielādē LAS datu karšu lapas...")
+
+    try:
+        # Lejupielādē ZIP failu no Google Drive
+        if download_from_google_drive(file_id, output_zip_path):
+            progress_percentage += 0.3
+            progress_bar.progress(progress_percentage)
+
+            # Izveido pagaidu direktoriju ZIP faila izsaiņošanai
+            extracted_folder = "LASMAP_extracted"
+            if os.path.exists(extracted_folder):
+                shutil.rmtree(extracted_folder)  # Dzēš, ja jau eksistē
+            os.makedirs(extracted_folder, exist_ok=True)
+
+            # Izsaiņo ZIP failu
+            try:
+                shutil.unpack_archive(output_zip_path, extracted_folder)
                 progress_percentage += 0.3
                 progress_bar.progress(progress_percentage)
+            except Exception as e:
+                st.error(f"Kļūda izsaiņojot ZIP failu: {e}")
 
-                # Izveido pagaidu direktoriju ZIP faila izsaiņošanai
-                extracted_folder = "LASMAP_extracted"
-                if os.path.exists(extracted_folder):
-                    shutil.rmtree(extracted_folder)  # Dzēš, ja jau eksistē
-                os.makedirs(extracted_folder, exist_ok=True)
+            # Ielādē SHP failu
+            try:
+                shp_file_path = os.path.join(extracted_folder, 'LASMAP.shp')
+                gdf = gpd.read_file(shp_file_path)
+                progress_percentage += 0.4
+                progress_bar.progress(progress_percentage)
+            except Exception as e:
+                st.error(f"Kļūda SHP faila ielādē: {e}")
 
-                # Izsaiņo ZIP failu
-                try:
-                    shutil.unpack_archive(output_zip_path, extracted_folder)
-                    progress_percentage += 0.3
-                    progress_bar.progress(progress_percentage)
-                except Exception as e:
-                    st.error(f"Kļūda izsaiņojot ZIP failu: {e}")
+            # Lietotājam piedāvā augšupielādēt SHP komponentes failus (SHP, SHX, DBF)
+            uploaded_shp = st.file_uploader("Augšupielādē savu kontūras SHP failu komponentes (SHP, SHX, DBF)", type=["shp", "shx", "dbf"], accept_multiple_files=True)
 
-                # Ielādē SHP failu
-                try:
-                    shp_file_path = os.path.join(extracted_folder, 'LASMAP.shp')
-                    gdf = gpd.read_file(shp_file_path)
-                    progress_percentage += 0.4
-                    progress_bar.progress(progress_percentage)
-                except Exception as e:
-                    st.error(f"Kļūda SHP faila ielādē: {e}")
-
-                # Lietotājam piedāvā augšupielādēt SHP komponentes failus (SHP, SHX, DBF)
-                uploaded_shp = st.file_uploader("Augšupielādē savu kontūras SHP failu komponentes (SHP, SHX, DBF)", type=["shp", "shx", "dbf"], accept_multiple_files=True)
-
-                if uploaded_shp and len(uploaded_shp) == 3:
+            if uploaded_shp:
+                uploaded_files = {file.type.split('/')[1]: file for file in uploaded_shp}
+                if all(ext in uploaded_files for ext in ['shp', 'shx', 'dbf']):
                     start_button = st.button("Sākt")
 
                     if start_button:
                         with TemporaryDirectory() as temp_dir:
                             # Saglabāt visus augšupielādētos failus pagaidu mapē
-                            for uploaded_file in uploaded_shp:
-                                output_path = os.path.join(temp_dir, uploaded_file.name)
+                            for ext, uploaded_file in uploaded_files.items():
+                                output_path = os.path.join(temp_dir, f"uploaded.{ext}")
                                 with open(output_path, 'wb') as f:
                                     f.write(uploaded_file.getbuffer())
 
                             # Ielādē kontūras SHP failu
-                            shp_file_path = [f.name for f in uploaded_shp if f.name.endswith('.shp')][0]
-                            shp_file_path = os.path.join(temp_dir, shp_file_path)
+                            shp_file_path = os.path.join(temp_dir, 'uploaded.shp')
 
                             try:
                                 contour_gdf = gpd.read_file(shp_file_path)
@@ -153,12 +179,12 @@ if st.button("Pieslēgties"):
                                 matched_polygons = 0  # Skaitīt pārklājušos poligonus
                                 links = []  # Saglabāt saites
 
-                                # Progresjoslas izveide
+                                # Progresijoslas izveide
                                 progress_bar = st.progress(0)
                                 progress_percentage = 0
 
                                 for index, row in gdf.iterrows():
-                                    if 'link' in row and row['link']:  # Pārbaudīt, vai ir "link" atribūts
+                                    if 'link' in row and pd.notna(row['link']):  # Pārbaudīt, vai ir "link" atribūts
                                         polygon = row.geometry
                                         # Pārbaudīt pārklāšanos ar kontūru faila ģeometriju
                                         if contour_gdf.intersects(polygon).any():
@@ -166,7 +192,7 @@ if st.button("Pieslēgties"):
                                             link = row['link']
                                             links.append(link)  # Saglabāt saiti sarakstā
 
-                                            # Atjauno progresjoslu
+                                            # Atjauno progresijoslu
                                             progress_percentage = (index + 1) / total_polygons
                                             progress_bar.progress(progress_percentage)
 
@@ -186,9 +212,13 @@ if st.button("Pieslēgties"):
                                 st.error(f"Kļūda, ielādējot kontūras SHP failu: {e}")
                 else:
                     st.warning("Lūdzu, augšupielādē SHP, SHX un DBF failus vienlaikus.")
-            else:
-                st.error("Kļūda ZIP faila lejupielādē no Google Drive.")
-        except Exception as e:
-            st.error(f"Kļūda: {e}")
-    else:
-        st.error("Nepareizs lietotājvārds vai parole.")
+        else:
+            st.error("Kļūda ZIP faila lejupielādē no Google Drive.")
+    except Exception as e:
+        st.error(f"Kļūda: {e}")
+
+# Galvenais izpildes punkts
+if st.session_state.authenticated:
+    main_app()
+else:
+    login()
